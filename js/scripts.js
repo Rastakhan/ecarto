a5dbd462d70a08052711ee50da2ccef7f4dd2c5a
Rastakhan <authentifiziert@gmail.com>
2021-02-08 04:45:21 -0600

    almost 100% working, easier if i get map names from an api then i could put to work titles at -> cartodata/

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,29 @@
 window.onload = () => {
 
-    const maps = document.querySelectorAll('img.mapi');
+    const maps = document.querySelectorAll('div.collapse-map-img > img');
     const aside = document.getElementById('asideMenu');
-    const mapi = document.getElementById('mapi');
-    pixels = document.body.clientWidth;
-    console.log(pixels);
+    const mapi = document.getElementById('mapid');
+    const header = document.getElementById('headernav');
+    const title = document.getElementById('mapTitle');
+    const ecarto = document.getElementById('ecarto');
+
+
+    var pixels = document.body.clientWidth;
+    var heigth = document.body.clientHeight;
 
     maps.forEach(x => {
         x.addEventListener("click", setMap);
 
     })
 
+    title.addEventListener("click", hide_menu);
+    ecarto.addEventListener("click", hide_menu);
+
     function setMap() {
-        console.log('aqui');
+        mapi.style.height = heigth - header.offsetHeight + 'px';
+
+        hide_menu();
+
         var mymap = L.map('mapid').setView([51.505, -0.09], 13);
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -20,7 +31,18 @@ window.onload = () => {
             id: 'mapbox/streets-v11',
             tileSize: 512,
             zoomOffset: -1,
-            accessToken: 'pk.eyJ1IjoicmFzdGFraGFuIiwiYSI6ImNra3ZtY2ttNzRrbGsydmxhdzgwbWh6OXEifQ.3bmNBEAwtcB2hp51OWGOuA'
+            accessToken: 'pk.eyJ1IjoicmFzdGFraGFuIiwiYSI6ImNra3ZtY2ttNzRrbGsydmxhdzgwbWh6OXEifQ.3bmNBEAwtcB2hp51OWGOuA' // Esto deberia estar en .ENV (IMPORTANTE!!!)
         }).addTo(mymap);
+    }
+
+    function hide_menu() {
+        if (pixels < 992) {
+            aside.classList.toggle('aside-menu-hide');
+            aside.classList.toggle('hide');
+
+            mapi.classList.toggle('mapwidth');
+            mapi.classList.toggle('col-12');
+
+        }
     }
 }
